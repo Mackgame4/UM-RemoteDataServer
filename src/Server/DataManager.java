02a194342d64601746e056d75c105c855c402fa1
Mack <42896437+Mackgame4@users.noreply.github.com>
2024-12-22 20:28:29 +0000

    feat(DataManager): Started DataManager

--- src/Server/DataManager.java
+++ src/Server/DataManager.java
@@ -7,4 +7,79 @@ import java.util.HashMap;
 public class DataManager {
     private static Map<String, String> data = new HashMap<>();
     private static ReentrantLock lock = new ReentrantLock();
+
+    public static void put(String key, String value) {
+        lock.lock();
+        try {
+            data.put(key, value);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static String get(String key) {
+        lock.lock();
+        try {
+            return data.get(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void remove(String key) {
+        lock.lock();
+        try {
+            data.remove(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void multiPut(Map<String, byte[]> pairs) {
+        lock.lock();
+        try {
+            for (Map.Entry<String, byte[]> pair : pairs.entrySet()) {
+                data.put(pair.getKey(), new String(pair.getValue()));
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Map<String, String> multiGet(String[] keys) {
+        lock.lock();
+        try {
+            Map<String, String> result = new HashMap<>();
+            for (String key : keys) {
+                result.put(key, data.get(key));
+            }
+            return result;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void multiRemove(String[] keys) {
+        lock.lock();
+        try {
+            for (String key : keys) {
+                data.remove(key);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public byte[] getWhen(String key, String keyCond, byte[] valueCond) {
+        lock.lock();
+        try {
+            String value = data.get(keyCond);
+            if (value != null && value.equals(new String(valueCond))) {
+                return data.get(key).getBytes();
+            }
+            return null;
+        } finally {
+            lock.unlock();
+        }
+    }
 }
