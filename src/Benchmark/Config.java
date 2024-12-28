163a63f666b1479fb892bee9d7396437375d8bc1
Mack <42896437+Mackgame4@users.noreply.github.com>
2024-12-28 01:14:54 +0000

    Update Config.java

--- src/Benchmark/Config.java
+++ src/Benchmark/Config.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Config {
     public static final String PATH = "data.txt";
     public static boolean RUN_THREADED = true; // TODO: save results to a file and show them in a GUI graph
+    // TODO: add more scenarios; add ram, cpu and disk usage monitoring on the server and client
 
     // Define benchmark scenarios
     public static final List<BenchmarkScenario> benchmarks = Arrays.asList(
